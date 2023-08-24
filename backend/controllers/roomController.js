884f2b9a2ed36afae32a8b27ee3bcaf6444ac821
yulian5kov <yulian5kov@gmail.com>
2023-08-24 14:20:32 +0300

    backend: fixed - formatted IDs | duplicateRoom | updateMemberRole

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,203 +1,281 @@
-const Room = require('../models/roomModel')
+const Room = require("../models/roomModel");
 
 // CREATE a room
-// GOOD
-const createRoom = async (req,res) => {
-    const { title, members } = req.body;
-    const creatorId = req.user._id; // Logged-in user's ID
+const createRoom = async (req, res) => {
+  const { title, members } = req.body;
+  const creatorId = req.user._id; // Logged-in user's ID
 
-    let emptyFields = []
-    if(!title){
-        emptyFields.push('title')
-    }
-    if(emptyFields.length > 0){
-        return res.status(400).json({error: "Please fill in all the fields", emptyFields})
-    }
-
-    let roomMembers = [{ user: creatorId, role: 'owner' }]; // Always include the creator as owner
-
-    if (members && Array.isArray(members)) {
-      // If members array is provided and is an array
-      members.forEach(member => {
-          if (member.user && member.role) {
-              roomMembers.push(member);
-          }
-      });
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
   }
 
-    // add to the database
-    try {
-        const room = await Room.create({ 
-            title,
-            members: roomMembers
-        })
-        res.status(200).json(room)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+  let roomMembers = [{ user: creatorId, role: "owner" }]; // Always include the creator as owner
 
-}
+  if (members && Array.isArray(members)) {
+    // If members array is provided and is an array
+    members.forEach((member) => {
+      if (member.user && member.role) {
+        roomMembers.push(member);
+      }
+    });
+  }
+
+  // add to the database
+  try {
+    const room = await Room.create({
+      title,
+      members: roomMembers,
+    });
+
+    const formattedRoom = {
+      roomID: room._id,
+      title: room.title,
+      members: room.members.map((member) => ({
+        userID: member.user._id,
+        username: member.user.username,
+        memberID: member._id,
+        role: member.role,
+      })),
+      __v: room.__v,
+    };
+
+    res.status(200).json(formattedRoom);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // DELETE a room
-// GOOD
 const deleteRoom = async (req, res) => {
   const roomId = req.params.roomId;
   const userId = req.user._id;
 
   try {
-      const foundRoom = await Room.findById(roomId); // Rename the variable here
+    const foundRoom = await Room.findById(roomId); // Rename the variable here
 
-      if (!foundRoom) {
-        return res.status(404).json({ error: 'Room not found' });
-      }
+    if (!foundRoom) {
+      return res.status(404).json({ error: "Room not found" });
+    }
 
-      // Find the member in the room with the given user ID
-      const memberWithUserId = foundRoom.members.find(member => {
-        return member.user._id.toString() === userId.toString();
-      });
-      
-      if (!memberWithUserId) {
-        return res.status(403).json({ error: 'You do not have permission to delete this room' });
-      }
+    // Find the member in the room with the given user ID
+    const memberWithUserId = foundRoom.members.find((member) => {
+      return member.user._id.toString() === userId.toString();
+    });
 
-      const isOwner = memberWithUserId.role === 'owner';
-      
-      if (isOwner) {
-        await foundRoom.deleteOne();
-        return res.status(200).json({ message: 'Room deleted successfully' });
-      } else {
-        return res.status(403).json({ error: 'You do not have permission to delete this room' });
-      }
+    if (!memberWithUserId) {
+      return res
+        .status(403)
+        .json({ error: "You do not have permission to delete this room" });
+    }
+
+    const isOwner = memberWithUserId.role === "owner";
+
+    if (isOwner) {
+      await foundRoom.deleteOne();
+      return res.status(200).json({ message: "Room deleted successfully" });
+    } else {
+      return res
+        .status(403)
+        .json({ error: "You do not have permission to delete this room" });
+    }
   } catch (error) {
-      res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
-// DUPLICATE a room
-// GOOD
+// POST DUPLICATED room
 const duplicateRoom = async (req, res) => {
-  const roomIdToDuplicate = req.params.roomId;
-  const changes = req.body;
+  const { roomId } = req.params;
+  // const { body } = req;
 
   try {
-      // Find the room to duplicate
-      const roomToDuplicate = await Room.findById(roomIdToDuplicate);
+    const originalRoom = await Room.findById(roomId);
 
-      if (!roomToDuplicate) {
-          return res.status(404).json({ error: 'Room not found' });
-      }
+    if (!originalRoom) {
+      return res.status(404).json({ error: "Room not found" });
+    }
 
-      // Duplicate the room with optional changes
-      const duplicatedRoom = new Room({
-          title: changes.title || roomToDuplicate.title, // Keep original title if not changed
-          members: roomToDuplicate.members.map(member => ({
-              user: member.user,
-              role: member.role
-          })), // Duplicate members as well
-          ...changes // Apply other changes from the request body
-      });
+    const loggedInUserId = req.user._id; // Logged-in user's ID
+    const loggedInUserIsOwner = originalRoom.members.some(
+      (member) => member.user.equals(loggedInUserId) && member.role === "owner"
+    );
 
-      await duplicatedRoom.save();
+    if (!loggedInUserIsOwner) {
+      return res
+        .status(403)
+        .json({ error: "You're not authorized to duplicate this room" });
+    }
 
-      res.status(200).json(duplicatedRoom);
+    const duplicatedTitle = `Copy of ${originalRoom.title}`;
+    const duplicatedMembers = [...originalRoom.members]; // Copy the members array
+
+    const newRoom = await Room.create({
+      title: duplicatedTitle,
+      members: duplicatedMembers,
+    });
+
+    const formattedRoom = {
+      roomID: newRoom._id,
+      title: newRoom.title,
+      members: newRoom.members.map((member) => ({
+        userID: member.user._id,
+        username: member.user.username,
+        memberID: member._id,
+        role: member.role,
+      })),
+      __v: newRoom.__v,
+    };
+
+    res.status(200).json(formattedRoom);
   } catch (error) {
-      res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
-
-// Add Members to a Room
-// GOOD
+// ADD Members to a Room
 const addMembersToRoom = async (req, res) => {
-    const { members } = req.body;
-    const roomId = req.params.roomId;
-  
-    try {
-      const room = await Room.findById(roomId)
-        .populate({
-        path: 'members.user',
-        select: 'username' // Fetch only the username field
-      })
-        .exec();
-  
-      if (!room) {
-        return res.status(404).json({ error: 'Room not found' });
-      }
-  
-      room.members.push(...members);
-      await room.save();
-  
-      res.status(200).json(room);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  const { members } = req.body;
+  const roomId = req.params.roomId;
+
+  try {
+    const room = await Room.findById(roomId);
+
+    if (!room) {
+      return res.status(404).json({ error: "Room not found" });
     }
+
+    room.members.push(...members);
+    await room.save();
+
+    const formattedRoom = {
+      roomID: room._id,
+      title: room.title,
+      members: room.members.map((member) => ({
+        userID: member.user._id,
+        username: member.user.username,
+        memberID: member._id,
+        role: member.role,
+      })),
+      __v: room.__v,
+    };
+
+    res.status(200).json(formattedRoom);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-// Update Member Role
-// GOOD 
+// UPDATE Member Role
 const updateMemberRole = async (req, res) => {
-    const { newRole } = req.body;
-    const { roomId, memberId } = req.params;
-  
-    try {
-      const room = await Room.findById(roomId);
-  
-      if (!room) {
-        return res.status(404).json({ error: 'Room not found' });
-      }
-  
-      const member = room.members.id(memberId);
-      if (!member) {
-        return res.status(404).json({ error: 'Member not found in room' });
-      }
-  
-      member.role = newRole;
-      await room.save();
-  
-      res.status(200).json(room);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  const { newRole } = req.body;
+  const { roomId, memberId } = req.params;
+  const loggedInUserId = req.user._id; // Assuming user ID is available in req.user
+
+  try {
+    const room = await Room.findById(roomId);
+    if (!room) {
+      return res.status(404).json({ error: "Room not found" });
     }
+
+    const member = room.members.find((member) =>
+      member.user._id.equals(loggedInUserId)
+    );
+    if (!member) {
+      return res.status(404).json({ error: "Member not found in room" });
+    }
+
+    // Check if the requester is the owner
+    // Check if the requester is the owner
+    if (member.role === "owner") {
+      const memberToUpdate = room.members.id(memberId);
+      if (!memberToUpdate) {
+        return res.status(404).json({ error: "Member not found in room" });
+      }
+
+      memberToUpdate.role = newRole;
+      await room.save();
+
+      const formattedRoom = {
+        roomID: room._id,
+        title: room.title,
+        members: room.members.map((member) => ({
+          userID: member.user._id,
+          username: member.user.username,
+          memberID: member._id,
+          role: member.role,
+        })),
+        __v: room.__v,
+      };
+
+      res.status(200).json(formattedRoom);
+    } else {
+      res.status(403).json({ error: "Only the owner can change roles" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
-  
-// Get Room Details
-// GOOD
+
+// GET Room Details
 const getRoomDetails = async (req, res) => {
   const roomId = req.params.roomId;
 
   try {
-      const room = await Room.findById(roomId)
-        .populate({
-          path: 'members.user',
-          select: 'username' // Fetch only the username field
-        })
-        .exec();
+    const room = await Room.findById(roomId);
 
-      if (!room) {
-        return res.status(404).json({ error: 'Room not found' });
-      }
+    if (!room) {
+      return res.status(404).json({ error: "Room not found" });
+    }
 
-      res.status(200).json(room);
+    const formattedRoom = {
+      roomID: room._id,
+      title: room.title,
+      members: room.members.map((member) => ({
+        userID: member.user._id,
+        username: member.user.username,
+        memberID: member._id,
+        role: member.role,
+      })),
+      __v: room.__v,
+    };
+
+    res.status(200).json(formattedRoom);
   } catch (error) {
-      res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
-// Get All Rooms
-// GOOD
+// GET All Rooms
 const getAllRooms = async (req, res) => {
-    try {
-      const rooms = await Room.find()
-        .populate('members.user', 'username') // Populate user's username from the 'User' collection
-        .exec();
-      res.status(200).json(rooms);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+  try {
+    const rooms = await Room.find();
+
+    // Create a new array with updated field names
+    const roomsWithUpdatedIDs = rooms.map((room) => ({
+      roomID: room._id,
+      title: room.title,
+      members: room.members.map((member) => ({
+        userID: member.user._id,
+        username: member.user.username,
+        memberID: member._id,
+        role: member.role,
+      })),
+      __v: room.__v,
+    }));
+
+    res.status(200).json(roomsWithUpdatedIDs);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // DELETE members (owner only)
-// GOOD
 const deleteMembers = async (req, res) => {
   const { membersToDelete } = req.body;
   const roomId = req.params.roomId;
@@ -206,27 +284,29 @@ const deleteMembers = async (req, res) => {
     const room = await Room.findById(roomId);
 
     if (!room) {
-      return res.status(404).json({ error: 'Room not found' });
+      return res.status(404).json({ error: "Room not found" });
     }
 
     // Check if the requester is the owner
     const requesterId = req.user._id;
-    const owner = room.members.find(member => member.role === 'owner');
+    const owner = room.members.find((member) => member.role === "owner");
     if (owner.user.toString() !== requesterId.toString()) {
-      return res.status(403).json({ error: 'Permission denied' });
+      return res.status(403).json({ error: "Permission denied" });
     }
 
     const deletedMembers = [];
     const notFoundMembers = [];
 
     for (const memberId of membersToDelete) {
-      const memberIndex = room.members.findIndex(member => member._id.toString() === memberId);
+      const memberIndex = room.members.findIndex(
+        (member) => member._id.toString() === memberId
+      );
       if (memberIndex !== -1) {
         const deletedMember = room.members.splice(memberIndex, 1)[0];
         deletedMembers.push({
           memberId: deletedMember._id,
           userId: deletedMember.user._id,
-          username: deletedMember.user.username
+          username: deletedMember.user.username,
         });
       } else {
         notFoundMembers.push(memberId);
@@ -235,26 +315,23 @@ const deleteMembers = async (req, res) => {
 
     await room.save();
 
-    res.status(200).json({ message: 'Members deleted successfully', deletedMembers, notFoundMembers });
+    res.status(200).json({
+      message: "Members deleted successfully",
+      deletedMembers,
+      notFoundMembers,
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-
-
-
-
-
-
-  
 module.exports = {
-    createRoom,
-    addMembersToRoom,
-    updateMemberRole,
-    getRoomDetails,
-    getAllRooms,
-    deleteRoom,
-    duplicateRoom,
-    deleteMembers
+  createRoom,
+  addMembersToRoom,
+  updateMemberRole,
+  getRoomDetails,
+  getAllRooms,
+  deleteRoom,
+  duplicateRoom,
+  deleteMembers,
 };
